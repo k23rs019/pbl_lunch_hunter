ffc490a409e7554e35dfa5325115f0d6f8133d79
Rs047-el <209739223+Rs047-el@users.noreply.github.com>
2025-11-24 10:09:16 +0900

    Update

--- src/user_check.php
+++ src/user_check.php
@@ -3,7 +3,7 @@ $user_id = $_POST['user_id'];
 $user_password=$_POST['user_password'];
 if($user_id = "kyusan" && $user_password = "pbl2025"){ //動作チェック用
     $_SESSION['user_id'] = $user_id;
-    $_SESSION['user_paswword'] = $user_paswword;
+    $_SESSION['user_password'] = $user_password;
     $_SESSION['usertype_id'] = 1;
     header('Location:index.php');
 } else {
--- src/user_login.php
+++ src/user_login.php
@@ -1,9 +1,9 @@
-<from action="?do=user_check" method="post">
+<form action="?do=user_check" method="post">
     <table>
-        <tr><td>ユーザー名：</td><td><input type="text" name="user_id"></td></tr>
-        <tr><td>パスワード：</td><td><input type="text" name="user_id"></td></tr>
+        <tr><td>ユーザ名：</td><td><input type="text" name="user_id"></td></tr>
+        <tr><td>パスワード：</td><td><input type="password" name="user_password"></td></tr>
         <tr><td></td><td>
             <input type="submit" value="送信">&nbsp;<input type="reset" vale="取消">
         </td></tr>
     </table>
-</from>
+</form>
